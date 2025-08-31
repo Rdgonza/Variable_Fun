021c92cd595de6f6987f9ad3c4913ca9ef5c7f2f
Rdgonza <rgonzalez191@mail.valenciacollege.edu>
2025-08-31 14:06:34 -0400

    Add files via upload

---
+++ Variable_Fun.py
@@ -0,0 +1,14 @@
+multi_results = 5 * 6
+division_results = 8 / 3
+division_without_remainder = 8 // 3
+division_modulo = 8 % 3
+add_results = 4200000 + 23090
+subtract_results = 4200000 - 7633
+print("The answer to your query is", multi_results )
+print("The answer to your query is", round(division_results, 8))
+print("The answer to your query is", division_without_remainder)
+print("The answer to your query is", division_modulo)
+print("The answer to your query is", add_results)
+print("The answer to your query is", subtract_results)
+
+
