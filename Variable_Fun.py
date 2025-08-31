fc2e8dbd9dc3d893de89751b8849f55adb98fabd
Rdgonza <rgonzalez191@mail.valenciacollege.edu>
2025-08-31 14:27:35 -0400

    Add files via upload

--- Variable_Fun.py
+++ Variable_Fun.py
@@ -4,11 +4,17 @@ division_without_remainder = 8 // 3
 division_modulo = 8 % 3
 add_results = 4200000 + 23090
 subtract_results = 4200000 - 7633
+print("5 multiplied by 6")
 print("The answer to your query is", multi_results )
+print("8 divided by 3")
 print("The answer to your query is", round(division_results, 8))
+print("8 divided by 3 without the remainder")
 print("The answer to your query is", division_without_remainder)
+print("8 modulo 3 (remainder only)")
 print("The answer to your query is", division_modulo)
+print("4200000 add 23090")
 print("The answer to your query is", add_results)
+print("4200000 subtract 7633")
 print("The answer to your query is", subtract_results)
 
 
