afaef575f73256272ca397f05a5c6097678c0941
Rdgonza <rgonzalez191@mail.valenciacollege.edu>
2025-09-02 22:30:14 -0400

    Add files via upload

--- Variable_Fun.py
+++ Variable_Fun.py
@@ -1,21 +1,33 @@
-multi_results = 5 * 6
-division_results = 8 / 3
-division_without_remainder = 8 // 3
-division_modulo = 8 % 3
-add_results = 4200000 + 23090
-subtract_results = 4200000 - 7633
-print("5 multiplied by 6")
-print("The answer to your query is", multi_results )
-print("8 divided by 3")
+# 5 multiplied by 6
+a = int(input("Please enter the first multiplication input: "))
+b = int(input("Please enter the second multiplication input: "))
+multi_results = a * b
+print("The answer to your query is:", multi_results)
+# 8 divided by 3
+c = int(input("Please enter the first division input: "))
+d = int(input("Please enter the second division input: "))
+division_results = c / d
 print("The answer to your query is", round(division_results, 8))
-print("8 divided by 3 without the remainder")
-print("The answer to your query is", division_without_remainder)
-print("8 modulo 3 (remainder only)")
-print("The answer to your query is", division_modulo)
-print("4200000 add 23090")
-print("The answer to your query is", add_results)
-print("4200000 subtract 7633")
-print("The answer to your query is", subtract_results)
-print("Robert Gonzalez")
-
-
+# 8 divided by 3 without the remainder (integer division)
+e = int(input("Please enter the first division without remainder input: "))
+f = int(input("Please enter the second division without remainder input: "))
+division_without_remainder = e // f
+print("The answer to your query is:", division_without_remainder)
+# 8 modulo 3 (remainder only)
+g = int(input("Please enter the first modulo input: "))
+h = int(input("Please enter the second modulo input: "))
+division_modulo = g % h
+print("The answer to your query is:", division_modulo)
+# 4200000 add 23090
+r = int(input("Please enter first addition input: "))
+j = int(input("Please enter second addition input: "))
+add_results = r + j
+print("The answer to your query is:", add_results)
+# 4200000 subtract 7633
+k = int(input("Please enter first subtraction input: "))
+y = int(input("Please enter second subtraction input: "))
+subtract_results = k - y
+print("The answer to your query is:", subtract_results)
+# Your name on the last line
+print(" This greatness was written by Robert Gonzalez")
+print("P.S. Thank you for allowing me to fix it!")
